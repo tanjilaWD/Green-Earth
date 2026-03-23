796ed265e453b68d174adea20e14d402714b0bb6
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-03-23 20:54:24 +0600

    card added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const CategoriesContainer = document.getElementById('CategoriesContainer');
+const treesContainer = document.getElementById('treesContainer');
 
 async function loadCategories(){
        //async await
@@ -19,4 +20,39 @@ async function loadCategories(){
        
 
     }
+
+async function loadTrees(){
+     const res = await fetch('https://openapi.programming-hero.com/api/plants');
+     const data = await res.json();
+     displayTrees(data.plants);
+    }
+
+function displayTrees(trees){
+    console.log(trees);
+    trees.forEach((tree) =>{
+        console.log(tree);
+        const card = document.createElement('div');
+        card.className = 'card bg-white shadow-sm';
+        card.innerHTML = `
+        <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p class="line-clamp-2">A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="badge badge-success">Success</div>
+    <div class="card-actions justify-between items-center">
+        <h2 class="font-bold  text-[#4ade80]">$500</h2>
+      <button class="btn btn-primary ">Buy Now</button>
+    </div>
+  </div> `;
+
+   treesContainer.appendChild(card);
+    });
+
+};
+
 loadCategories();
+loadTrees();
